cf12193caa934a043e38078d7b660f2acd9fa75c
romain.kuzniak <romain.kuzniak@openclassrooms.com>
2017-10-03 16:08:42 +0200

    wip

---
+++ src/Commands/Command.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OpenClassrooms\CodeGenerator\Commands;
+
+/**
+ * @author Romain Kuzniak <romain.kuzniak@openclassrooms.com>
+ */
+class Command
+{
+    public static function main()
+    {
+
+        print ('yes');
+    }
+}
