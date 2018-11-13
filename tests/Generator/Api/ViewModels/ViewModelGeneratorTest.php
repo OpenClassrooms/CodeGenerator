0c4aad2693aa22b683af3b08e2ae1a39f0368278
Samuel Gomis <gomis.samuel@gmail.com>
2018-11-13 15:44:18 +0100

    test(ViewModels) extract some setup stuff in abstract class

--- tests/Generator/Api/ViewModels/ViewModelGeneratorTest.php
+++ tests/Generator/Api/ViewModels/ViewModelGeneratorTest.php
@@ -2,25 +2,20 @@
 
 namespace OpenClassrooms\CodeGenerator\Generator\Tests\Api\ViewModels;
 
-use OpenClassrooms\CodeGenerator\FileObjects\Impl\UseCaseResponseFileObjectFactoryImpl;
-use OpenClassrooms\CodeGenerator\FileObjects\Impl\ViewModelFileObjectFactoryImpl;
 use OpenClassrooms\CodeGenerator\Generator\Api\ViewModels\DTO\Request\ViewModelGeneratorRequestBuilderImpl;
 use OpenClassrooms\CodeGenerator\Generator\Api\ViewModels\Request\ViewModelGeneratorRequestBuilder;
 use OpenClassrooms\CodeGenerator\Generator\Api\ViewModels\ViewModelGenerator;
-use OpenClassrooms\CodeGenerator\Services\Impl\FieldObjectServiceImpl;
 use OpenClassrooms\CodeGenerator\SkeletonModels\Api\ViewModel\Impl\ViewModelSkeletonModelAssemblerImpl;
 use OpenClassrooms\CodeGenerator\Tests\Doubles\FileObjects\Api\ViewModels\ViewModel\ViewModelFileObjectStub1;
 use OpenClassrooms\CodeGenerator\Tests\Doubles\FileObjects\FileObjectTestCase;
 use OpenClassrooms\CodeGenerator\Tests\Doubles\Gateways\FileObject\InMemoryFileObjectGateway;
-use OpenClassrooms\CodeGenerator\Tests\Doubles\Services\Templating\TemplatingMock;
 use OpenClassrooms\CodeGenerator\Tests\Fixtures\Classes\BusinessRules\UseCases\Domain\SubDomain\DTO\Response\FunctionalEntityResponseDTO;
-use OpenClassrooms\CodeGenerator\Tests\Fixtures\FixturesConfig;
-use PHPUnit\Framework\TestCase;
+use OpenClassrooms\CodeGenerator\Tests\Generator\Api\ViewModels\AbstractViewModelGeneratorTestCase;
 
 /**
  * @author Samuel Gomis <samuel.gomis@external.openclassrooms.com>
  */
-class ViewModelGeneratorTest extends TestCase
+class ViewModelGeneratorTest extends AbstractViewModelGeneratorTestCase
 {
     use FileObjectTestCase;
 
@@ -37,7 +32,7 @@ class ViewModelGeneratorTest extends TestCase
     /**
      * @test
      */
-    public function generate()
+    public function generate_ReturnFileObject()
     {
         $actualFileObject = $this->viewModelGenerator->generate($this->request);
 
@@ -57,15 +52,8 @@ class ViewModelGeneratorTest extends TestCase
             ->build();
 
         $this->viewModelGenerator = new ViewModelGenerator();
-        $viewModelFileObjectFactory = new ViewModelFileObjectFactoryImpl();
-        $viewModelFileObjectFactory->setBaseNamespace(FixturesConfig::BASE_NAMESPACE);
-        $useCaseResponseFileObjectFactory = new UseCaseResponseFileObjectFactoryImpl();
-        $useCaseResponseFileObjectFactory->setBaseNamespace(FixturesConfig::BASE_NAMESPACE);
-        $this->viewModelGenerator->setUseCaseResponseFileObjectFactory($useCaseResponseFileObjectFactory);
-        $this->viewModelGenerator->setViewModelFileObjectFactory($viewModelFileObjectFactory);
-        $this->viewModelGenerator->setFileObjectGateway(new InMemoryFileObjectGateway());
-        $this->viewModelGenerator->setFieldObjectService(new FieldObjectServiceImpl());
-        $this->viewModelGenerator->setTemplating(new TemplatingMock());
+        $this->buildViewModelGenerator($this->viewModelGenerator);
+
         $this->viewModelGenerator->setViewModelSkeletonModelAssembler(new ViewModelSkeletonModelAssemblerImpl());
     }
 }
