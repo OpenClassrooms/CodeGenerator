3399cca1ef69087b836ddc2e12f12f35b0c78403
Samuel <samuel.gomis@openclassrooms.com>
2018-12-26 11:56:14 +0100

    feat(ViewModelListItemGenerator) OT-9 implement generator, mocks and unit tests

---
+++ src/SkeletonModels/Api/ViewModel/ViewModelListItemSkeletonModel.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace OpenClassrooms\CodeGenerator\SkeletonModels\Api\ViewModel;
+
+use OpenClassrooms\CodeGenerator\SkeletonModels\AbstractSkeletonModel;
+
+/**
+ * @author Samuel Gomis <samuel.gomis@external.openclassrooms.com>
+ */
+abstract class ViewModelListItemSkeletonModel extends AbstractSkeletonModel
+{
+    public $parentShortName;
+
+    public $templatePath = 'Api/ViewModels/ViewModelListItem.php.twig';
+}
