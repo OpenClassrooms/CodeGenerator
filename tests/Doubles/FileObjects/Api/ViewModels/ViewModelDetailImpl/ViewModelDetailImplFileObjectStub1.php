c65aa178a6465a1dfd2bd07068c5246434b897e9
Samuel Gomis <gomis.samuel@gmail.com>
2018-12-26 11:57:40 +0100

    feat(ViewModelDetailImplGenerator) OT-5 implement generator, mocks and unit tests

---
+++ tests/Doubles/FileObjects/Api/ViewModels/ViewModelDetailImpl/ViewModelDetailImplFileObjectStub1.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace OpenClassrooms\CodeGenerator\Tests\Doubles\FileObjects\Api\ViewModels\ViewModelDetailImpl;
+
+use OpenClassrooms\CodeGenerator\FileObjects\FileObject;
+use OpenClassrooms\CodeGenerator\Tests\Fixtures\Classes\tests\Doubles\Api\ViewModels\Domain\SubDomain\FunctionalEntityDetailStub1;
+
+/**
+ * @author Samuel Gomis <gomis.samuel@external.openclassrooms.com>
+ */
+class ViewModelDetailImplFileObjectStub1 extends FileObject
+{
+    const CLASS_NAME = 'OpenClassrooms\CodeGenerator\Tests\Fixtures\Classes\Api\ViewModels\Domain\SubDomain\Impl\FunctionalEntityDetailImpl';
+
+    public function __construct()
+    {
+        $this->content = __DIR__ . '/../../../../../Fixtures/Classes/Api/ViewModels/Domain/SubDomain/Impl/FunctionalEntityDetailImpl.php';
+        $this->className = self::CLASS_NAME;
+    }
+}
