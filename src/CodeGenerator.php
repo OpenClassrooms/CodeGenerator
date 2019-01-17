b9897f7f02b71e0adc74ccaddecce466fdca178f
Samuel Gomis <gomis.samuel@gmail.com>
2019-01-17 14:53:32 +0100

    fix CodeGenerator.php

--- src/CodeGenerator.php
+++ src/CodeGenerator.php
@@ -1,7 +1,12 @@
 #!/bin/php
 
 <?php
-foreach (array(__DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php') as $file) {
+foreach (array(
+             __DIR__ . '/../../../autoload.php',
+             __DIR__ . '/../../autoload.php',
+             __DIR__ . '/../vendor/autoload.php',
+             __DIR__ . '/vendor/autoload.php',
+         ) as $file) {
     if (file_exists($file)) {
         define('CODE_GENERATOR_COMPOSER_INSTALL', $file);
 
