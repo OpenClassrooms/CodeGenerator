e0515a50d3447c23b760acc4dec656e6d2ee422f
Samuel Gomis <gomis.samuel@gmail.com>
2019-01-18 13:57:42 +0100

    fix(ViewModelCommandTest) use mock to test command

---
+++ tests/Doubles/Commands/ViewModelCommandMock.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace OpenClassrooms\CodeGenerator\Tests\Doubles\Commands;
+
+use OpenClassrooms\CodeGenerator\Commands\ViewModelCommand;
+
+/**
+ * @author Samuel Gomis <gomis.samuel@external.openclassrooms.com>
+ */
+class ViewModelCommandMock extends ViewModelCommand
+{
+    const ROOT_DIR = __DIR__ . '/../../..';
+}
