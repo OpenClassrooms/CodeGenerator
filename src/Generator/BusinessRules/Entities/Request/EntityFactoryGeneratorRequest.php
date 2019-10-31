56f2b92eb4997d1cc47d02420406a66f12735f07
Samuel Gomis <gomis.samuel@gmail.com>
2019-10-31 14:48:26 +0100

    feat(EntityFactory) create generator and unit test

---
+++ src/Generator/BusinessRules/Entities/Request/EntityFactoryGeneratorRequest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace OpenClassrooms\CodeGenerator\Generator\BusinessRules\Entities\Request;
+
+use OpenClassrooms\CodeGenerator\Generator\GeneratorRequest;
+
+interface EntityFactoryGeneratorRequest extends GeneratorRequest
+{
+    public function getEntityClassName(): string;
+}
---
+++ src/SkeletonModels/BusinessRules/Entities/EntityFactorySkeletonModel.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace OpenClassrooms\CodeGenerator\SkeletonModels\BusinessRules\Entities;
+
+use OpenClassrooms\CodeGenerator\SkeletonModels\AbstractSkeletonModel;
+
+abstract class EntityFactorySkeletonModel extends AbstractSkeletonModel
+{
+    public $entityShortName;
+
+    public $templatePath = 'BusinessRules\Entities\EntityFactory.php.twig';
+}
